fec8966093b08bfd9002710f3f6465e86038390c
alexbj <ajoveski@gmail.com>
2025-10-27 15:47:18 +0100

    Add express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello!");
+});
+
+app.listen(5000, () =>
+	console.log("Server is listening on http://localhost:5000...")
+);
