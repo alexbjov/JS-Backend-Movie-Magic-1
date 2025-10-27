92790d904144d60bd6f0596493b7b589bb30e51b
alexbj <ajoveski@gmail.com>
2025-10-27 21:24:12 +0100

    Add Not found page

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ import movieController from "./controllers/movieController.js";
 const routes = Router();
 routes.use(homeController);
 routes.use("/movies", movieController);
-
+routes.get("/*splat", (req, res) => {
+	res.render("404");
+});
 export default routes;
