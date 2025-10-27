0c18042fb90ec4092cbe601538eb6cf10bb01673
alexbj <ajoveski@gmail.com>
2025-10-27 23:59:02 +0100

    Show static Search page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -19,4 +19,8 @@ movieController.get("/:movieId/details", (req, res) => {
 	res.render("details", { movie });
 });
 
+movieController.get("/search", (req, res) => {
+	res.render("search");
+});
+
 export default movieController;
