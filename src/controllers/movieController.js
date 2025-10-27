77a5053df0782b120ec330e31a3d2125839d43f1
alexbj <ajoveski@gmail.com>
2025-10-27 23:47:05 +0100

    Render movie details

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -16,8 +16,7 @@ movieController.post("/create", (req, res) => {
 movieController.get("/:movieId/details", (req, res) => {
 	const movieId = req.params.movieId;
 	const movie = movieService.getOne(movieId);
-	console.log(movie);
-	res.end();
+	res.render("details", { movie });
 });
 
 export default movieController;
